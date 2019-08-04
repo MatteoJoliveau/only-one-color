51190202804a547ff04a6138fd87992cc6fa826b
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-08-04 18:44:20 +0200

    Fixed main menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -6,6 +6,7 @@ onready var quit_button: Button = $Columns/Menu/Buttons/QuitButton
 func _ready():
 	new_game_button.connect("pressed", self, "_on_new_game")
 	quit_button.connect("pressed", self, "_on_quit")
+	new_game_button.grab_focus()
 
 func _on_new_game():
 	get_tree().change_scene("res://Game.tscn")
